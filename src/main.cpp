dcc6ab15a98fe62dac1a4249ed1a97d9a5ca1681
Kyle Evans <kevans91@ksu.edu>
2011-03-07 00:53:27 -0600

    + Complete reading functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 
 #include "args.h"
+#include "datatypes.h"
 #include "savefile.h"
 
 int main(int argc, char **argv) {
@@ -14,6 +16,7 @@ int main(int argc, char **argv) {
 			std::cout << "ERROR: Failed to load savefile." << std::endl;
 		} else {
 			std::cout << "Reading savefile..." << std::endl;
+			sfile.Read();
 		}
 	}
 	return 0;
--- src/savefile.h
+++ src/savefile.h
@@ -4,16 +4,93 @@
 #include <fstream>
 #include <string>
 #include <sys/stat.h>
+#include <vector>
 
+#include "entry.h"
 #include "netbuf.h"
+
+struct EntryComp {
+        bool operator()(Entry *lhs, Entry *rhs) {
+                return (lhs->Index() < rhs->Index());
+        }
+};
+
+struct Node {
+	unsigned int		id;
+	Node			*parent;
+	std::vector<Node *>	children;
+
+	std::string		name;
+	EntryData		*data;
+
+	Node(unsigned int nodeId, const std::string& nodeName, EntryData * nodeData) : id(nodeId), parent(NULL), name(nodeName), data(nodeData) {
+	}
+
+	~Node() {
+		delete data;
+		data = NULL;
+
+		for(std::vector<Node *>::iterator iter = children.begin(); iter != children.end(); ) {
+			Node * node = (*iter);
+			iter = children.erase(iter);
+			delete node;
+			node = NULL;
+		}
+	}
+
+	bool AddChild(Node *node) {
+		if(node != NULL) {
+			children.push_back(node);
+			node->parent = this;
+			return true;
+		}
+
+		return false;
+	}
+
+	std::string Dump(unsigned int level = 0) {
+		std::ostringstream retString;
+		std::string tabString = "";
+		for(unsigned int i = 0; i < level; i++) {
+			tabString += '\t';
+		}
+
+		if(!name.empty()) {
+			retString << tabString << name;
+			if(data != NULL) {
+				retString << " = " << data->ToString();
+			}
+		}
+
+		retString << '\n';
+
+		for(std::vector<Node *>::iterator iter = children.begin(); iter != children.end(); ++iter) {
+			retString << (*iter)->Dump(level + 1);
+		}
+		return retString.str();
+	}
+
+	Node *Find(unsigned int index) {
+		if(index == id) return this;
+		if(children.empty()) return NULL;
+		Node * returnNode = NULL;
+		for(std::vector<Node *>::iterator iter = children.begin(); iter != children.end(); ++iter) {
+			returnNode = (*iter)->Find(index);
+			if(returnNode != NULL) break;
+		}
+
+		return returnNode;
+	}
+};
 
 class Savefile {
 protected:
 private:
 	std::string	fileName;
 	Netbuf		*fileBuf;
+	Node		*tree;
 public:
-	Savefile(const std::string& name) : fileName(name), fileBuf(NULL) {
+	Savefile(const std::string& name) : fileName(name), fileBuf(NULL), tree(NULL) {
 		Load();
 	}
 
@@ -47,6 +124,8 @@ public:
 		fileName = name;
 		return Load();
 	}
+
+	bool Read();
 
 	inline bool operator !() const { return (!fileBuf || !(*fileBuf)); }
 };
